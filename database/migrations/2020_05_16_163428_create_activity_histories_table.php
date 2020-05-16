90683932a12457d166983123c3dcc9825fd2d434
hagios <hagioswilson@gmail.com>
2020-05-16 17:53:21 +0100

    Db design

---
+++ database/migrations/2020_05_16_163428_create_activity_histories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActivityHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activity_histories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('activity_id')->unsigned();
+            $table->string('request_ip');
+            $table->string('activity_update');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activity_histories');
+    }
+}
